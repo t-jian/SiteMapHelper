16b14023fbff3dcfa4ee0d60f5b4da7d6354efb8
t-jian <13768073860@163.com>
2022-09-24 17:33:05 +0800

    33444

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const app = express()
 var path = require('path')
-
 var router = require('./router')
 //数据解析中间件
 var bodyParser = require('body-parser')
-//用于生成 sitemap 文件
-var sitemap = require('sitemap-nodejs');
+
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+const { Server } = require("socket.io");
+const io = new Server();
 
 //设置静态路径。__dirname是当前文件所在绝对目录的意思。 使用static/xxxx
 app.use('/static',express.static(path.join(__dirname,'assets','assets')))
-
+app.use('/node_modules/',express.static(path.join(__dirname,'./node_modules/')))
 //告诉express框架使用express-art-template模板引擎渲染html后缀的模板文件
 app.engine('html',require('express-art-template'))
 //配置模板所在的位置
@@ -28,3 +30,13 @@ const server = app.listen(5000,function () {
     const {address,port} = server.address()
     console.log('HTTP服务启动成功: http://%s:%s',address,port)
 })
+
+io.on('connection',function(socket){
+    socket.emit("hello", "world");
+    eventEmitter.on("chageUrl",function(res){
+        console.log(res,5554546)
+        socket.emit("hello", res);
+    })
+})
+
+io.listen(server)
--- router.js
+++ router.js
@@ -1,6 +1,20 @@
 var express = require('express')
+const app = express()
 var router = express.Router()
+var siteMapHepler = require('./utils/siteMapHepler.js')
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+
 router.get('/',function (req,res){
+    console.log(eventEmitter,66666)
+    eventEmitter.emit('chageUrl',33333); 
+    siteMapHepler.init({
+        href:"http://www.ztan.net",
+        save_path:'sitemap.txt',
+        chageUrlCallback:(res)=>{
+            eventEmitter.emit('chageUrl',res); 
+        }
+    })
     res.render('index.html')
 })
 module.exports = router
